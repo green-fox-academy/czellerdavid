4edda8d4909b8d42295f83a086f7b72384675830
Dave <czellerdavid92@gmail.com>
2018-08-06 14:45:58 +0200

    stuff

---
+++ week_02/Day_01/src/Append.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Append {
+  public static void main(String[] args) {
+
+    // - Create a string variable named `typo` and assign the value `Chinchill` to it
+// - Write a function called `appendAFunc` that gets a string as an input,
+//   appends an 'a' character to its end and returns with a string
+//
+// - Print the result of `appendAFunc(typo)`
+
+
+    String typo = "Chinchill";
+    System.out.println(appendAFunc(typo));
+  }
+
+  public static String appendAFunc(String letter) {
+
+    letter += "a";
+    return letter;
+
+
+
+  }
+}
---
+++ week_02/Day_01/src/Doubling.java
@@ -0,0 +1,22 @@
+
+
+public class Doubling {
+  public static void main(String[] args) {
+    // - Create an integer variable named `baseNum` and assign the value `123` to it
+    // - Create a function called `doubling` that doubles it’s input parameter and returns with an integer
+    // - Print the result of `doubling(baseNum)`
+
+    int baseNum = 123;
+    int kicsi = 44;
+    System.out.println(doubling(baseNum));
+    System.out.println(doubling(kicsi));
+
+
+  }
+
+  public static int doubling(int jozsi) {
+    jozsi*=2;
+    return jozsi;
+  }
+
+}
