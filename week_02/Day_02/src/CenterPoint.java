a7a5515dae535d5fcd2ce3ac1df5a68eb9026781
Dave <czellerdavid92@gmail.com>
2018-08-08 16:29:41 +0200

    new3

---
+++ week_02/Day_02/src/CenterPoint.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static java.awt.image.ImageObserver.HEIGHT;
+import static java.awt.image.ImageObserver.WIDTH;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenterPoint {
+
+  public static void mainDraw(Graphics graphics) {
+    // Create a line drawing function that takes 2 parameters:
+    // The x and y coordinates of the line's starting point
+    // and draws a line from that point to the center of the canvas.
+    // Draw 3 lines with that function. Use loop for that.
+ for (int i = 10, j = 10; i <= 30; i += 10, j--)
+    graphics.drawLine(x, y  , j, i);
+  }
+
+  // Don't touch the code below
+  static int x = 160;
+  static int y = 40;
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+
+  static class ImagePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+    }
+  }
+}
