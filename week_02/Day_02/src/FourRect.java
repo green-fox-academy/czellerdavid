71bad961faab603ad3936f7a9be6ca3cd5ce65e6
Dave <czellerdavid92@gmail.com>
2018-08-09 13:40:05 +0200

    tadamm2

--- week_02/Day_02/src/FourRect.java
+++ week_02/Day_02/src/FourRect.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class FourRect {
+
+  public static void mainDraw(Graphics graphics){
+    // draw four different size and color rectangles.
+    // avoid code duplication.
+
+    for (int i = 10 , j = 10; i <= 50 ; i += 10, j =50) {
+//      Color c = new Color((int)Math.random()*255, (int)Math.random()*255, (int)Math.random()*255);
+//      graphics.setColor(c);
+      graphics.drawRect((int) (Math.random() * 320), (int) (Math.random() * 343), j, i);
+
+    }
+  }
+
+  //    Don't touch the code below
+  static int WIDTH = 320;
+  static int HEIGHT = 343;
+  public static void main(String[] args) {
+    JFrame jFrame = new JFrame("Drawing");
+    jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+    jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+    jFrame.add(new ImagePanel());
+    jFrame.setLocationRelativeTo(null);
+    jFrame.setVisible(true);
+  }
+  static class ImagePanel extends JPanel{
+    @Override
+    protected void paintComponent(Graphics graphics) {
+      super.paintComponent(graphics);
+      mainDraw(graphics);
+
+    }
+  }
+
 }
