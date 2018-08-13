8582477328896559e31d459cb8fe85fb43967222
Dave <czellerdavid92@gmail.com>
2018-08-13 10:28:42 +0200

    3rd

--- TrialExam/src/academy/greenfox/countas/CountAs.java
+++ TrialExam/src/academy/greenfox/countas/CountAs.java
@@ -7,18 +7,14 @@ public class CountAs {
   Scanner scanner = new Scanner(System.in);
     System.out.println("Enter a word: ");
   String operation = scanner.nextLine();
-  String firstLetter = "a";
+  String firstLetter = "a" + "A";
 
-  long l=System.nanoTime();
-    firstLetter = String.valueOf(operation.charAt(0));
-    System.out.println("1");
-
-
-
-    // Create a function that takes a string parameter,
-    // counts the occurances of the letter "a", and returns it as a number.
-
-    // example: on the input "Apple" the function should return 1 - print this result
-    // example: on the input "Blueberry" the function should return 0 - print this result
+    if (0 <= firstLetter.indexOf(operation.charAt(0))){
+      System.out.println(1);
+    }
+    else {
+      System.out.println(0);
+    }
   }
 }
+
