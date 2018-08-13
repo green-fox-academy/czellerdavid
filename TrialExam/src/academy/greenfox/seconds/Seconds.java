d8393a7fc7a76637f42f535343849b7f2c21c0d2
Dave <czellerdavid92@gmail.com>
2018-08-13 10:46:33 +0200

    almost

--- TrialExam/src/academy/greenfox/seconds/Seconds.java
+++ TrialExam/src/academy/greenfox/seconds/Seconds.java
@@ -2,10 +2,14 @@ package academy.greenfox.seconds;
 
 public class Seconds {
   public static void main(String[] args) {
+    int[] original = {1, 2, 3, 4, 5};
 
-
+    for (int i = 0; i < original.length; i++) {
+      System.out.println(original[i += 1]);
+    }
+  }
     // Create a function that takes a list as a parameter,
     // and returns a new list with every second element from the orignal list
     // example: [1, 2, 3, 4, 5] should produce [2, 4] - print this result
-  }
 }
+
