c64c21966f4b6b88e27e5da326b5478b4b43ffde
Dave <czellerdavid92@gmail.com>
2018-08-16 16:27:22 +0200

    stuff

---
+++ Week_03/Day_3/src/Bunny.java
@@ -0,0 +1,16 @@
+public class Bunny {
+  public static void main(String[] args) {
+
+    System.out.println(bunny(1));
+  }
+  public static int bunny(int n){
+
+    if (n == 0){
+      return 0;
+    }
+    return bunny ;
+  }
+}
+
+// We have a number of bunnies and each bunny has two big floppy ears.
+// We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
--- Week_03/Day_3/src/Power.java
+++ Week_03/Day_3/src/Power.java
@@ -1,11 +1,15 @@
 public class Power {
   public static void main(String[] args) {
 
+    System.out.println(count(3));
   }
   public static int count(int n){
-    int 
-  }
+    if(n == 0){
+      return 0;
+    }
 
+    return ((count(n-1) + (2*n)-1));
+  }
 }
 // Given base and n that are both 1 or more, compute recursively (no loops)
 // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
