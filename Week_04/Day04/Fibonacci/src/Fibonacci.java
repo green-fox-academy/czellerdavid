1631369e1c10c0706601033a3c8f3aea45562e8a
Dave <czellerdavid92@gmail.com>
2018-08-24 13:23:32 +0200

    fibi

--- Week_04/Day04/Fibonacci/src/Fibonacci.java
+++ Week_04/Day04/Fibonacci/src/Fibonacci.java
@@ -1,15 +1,15 @@
 public class Fibonacci {
 
-  public static void main (String[] args) {
+  public static void main(String[] args) {
 
     System.out.println(fib(10));
   }
 
-  public static int fib(int n){
+  public static int fib(int n) {
 
-    if (n == 1 || n == 2){
+    if (n == 1 || n == 2) {
       return 1;
     }
-    return n + fib(n-1) + fib(n-2);
+    return n + fib(n - 1) + fib(n - 2);
   }
 }
