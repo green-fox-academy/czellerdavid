3184128712ade453674833ab4269fbf49e7bd2d2
Dave <czellerdavid92@gmail.com>
2018-08-31 09:06:26 +0200

    taram

--- Week_05/cucc/Test/TrickyAverageTest.java
+++ Week_05/cucc/Test/TrickyAverageTest.java
@@ -6,22 +6,27 @@ import static org.junit.Assert.assertEquals;
 public class TrickyAverageTest {
 
 
-
-  int[] lofasz = {5, 2, 8, -1};
+  int[] number = {5, 2, 8, -1};
 
   @Test
   public void getTrickyAvg() {
 
-    System.out.println(lofasz);
-    assertEquals(3.5, TrickyAverage.getTrickyAvg(lofasz), 0);
-  }
-  @Test
-  public void maxAndEven(){
-
+    System.out.println(TrickyAverage.getTrickyAvg(number));
+    assertEquals(3.5, TrickyAverage.getTrickyAvg(number), 0);
   }
 
   @Test
-  public void minAndOdd(){
+  public void maxAndEven() {
+
+    System.out.println(TrickyAverage.maxAndEven(number));
+    assertEquals(8, TrickyAverage.maxAndEven(number), 0);
+  }
+
+  @Test
+  public void minAndOdd() {
+
+    System.out.println(TrickyAverage.minAndOdd(number));
+    assertEquals(-1, TrickyAverage.minAndOdd(number), 0);
 
   }
 }
