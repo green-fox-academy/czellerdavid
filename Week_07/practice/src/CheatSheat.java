7351c3860e91a7822fc8ea80f9e5c43752e35e0a
Dave <czellerdavid92@gmail.com>
2018-09-09 01:07:01 +0200

    kkm

---
+++ Week_07/practice/src/CheatSheat.java
@@ -0,0 +1,26 @@
+public class CheatSheat {
+  //member:
+  String name;
+  //constructor:
+  public CheatSheat(String name){
+    this.name = name;
+  }
+
+  //count:
+
+
+  public int getCount(){
+    int count = 0;
+
+    for (int i = 0; i < 10 ; i++) {
+
+//      if(i == valamivel){
+//        count++;
+//      }
+
+      count++;
+    }
+  }
+
+
+}
