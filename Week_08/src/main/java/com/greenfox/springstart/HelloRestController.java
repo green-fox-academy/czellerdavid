c2b6951ab3b03f317681e2d9d9876197ebb3b9bb
Dave <czellerdavid92@gmail.com>
2018-09-17 15:31:09 +0200

    3rdexercise

--- Week_08/src/main/java/com/greenfox/springstart/HelloRestController.java
+++ Week_08/src/main/java/com/greenfox/springstart/HelloRestController.java
@@ -1,17 +1,22 @@
 package com.greenfox.springstart;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloRestController {
 
-  @RequestMapping("/greeting")
-  public static Greeting greeting(){
-    Greeting greeting = new Greeting(1, "Lacika");
+
+  @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+  public static Greeting greeting(@RequestParam(value = "name") String name){
+
+    Greeting greeting = new Greeting(1, "Hello, " + name);
     return greeting;
 
   }
 
 }
+///@RequestMapping(value="/pow", method=RequestMethod.GET)
+//public int pow(@RequestParam(value="base") int base1, @RequestParam(value="ext") int ext1){
+//    int pow = (int) Math.pow(base1, ext1);
+//    return pow;
+//    }
