b5501b6b520da8ad83d053b0b6c7660c01fc4210
Dave <czellerdavid92@gmail.com>
2018-09-17 17:48:26 +0200

    Web Greet counter app

--- Week_08/src/main/java/com/greenfox/springstart/HelloWebController.java
+++ Week_08/src/main/java/com/greenfox/springstart/HelloWebController.java
@@ -3,11 +3,22 @@ package com.greenfox.springstart;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HelloWebController {
+
+  AtomicLong id;
+
+  public HelloWebController() {
+    this.id = new AtomicLong(1);
+  }
+
   @RequestMapping("/web/greeting")
   public String greeting(Model model) {
-    model.addAttribute("name", " World");
+    model.addAttribute("name", " Dave");
+    model.addAttribute("id", id.getAndIncrement());
     return "greeting";
   }
 }
